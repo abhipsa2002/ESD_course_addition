f80358c33a88f5586c531ad76bd48668b627bd88
abhipsapanda1 <pabhipsa02@gmail.com>
2023-11-26 00:20:26 +0530

    cors added

--- src/main/java/com/example/esdproj/Controller/specializationController.java
+++ src/main/java/com/example/esdproj/Controller/specializationController.java
@@ -1,15 +1,17 @@
 package com.example.esdproj.Controller;
 
 
-import com.example.esdproj.Entity.specialization;
 import com.example.esdproj.service.specializationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8081/")
+@CrossOrigin(origins = "http://localhost:8080/")
 @RestController
 @RequestMapping("/api/v1")
 public class specializationController {
